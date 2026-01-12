07251649310568283ca4580ff1ee0670e068effc
yamaguchikei <keep_casting.19860910@hotmail.co.jp>
2026-01-12 19:45:56 +0900

    コンポーネント表示制御

--- src/components/FlashMessage.tsx
+++ src/components/FlashMessage.tsx
@@ -1,18 +1,26 @@
-import { FiCheckCircle, FiX } from 'react-icons/fi';
+import { FiCheckCircle, FiX, FiXCircle } from 'react-icons/fi';
 import './FlashMessage.css';
+import { useUIStore } from '../modules/ui/ui.store';
 
 export default function FlashMessage() {
+  const { flashMessage, removeFlashMessage } = useUIStore();
+
+  if (!flashMessage) return null; //グローバルステートのflashMessageになにも入っていない場合はnullを返す。なにも表示されないということ。値が入ると下のコンポーネントが表示される。
+
   return (
     <div className='flash-message-container'>
-      <div className='flash-message flash-message--success'>
+      <div className={`flash-message flash-message--${flashMessage.type}`}>
         <div className='flash-message__icon'>
-          <FiCheckCircle />
+          {flashMessage.type === 'error' ? <FiXCircle /> : <FiCheckCircle />}
         </div>
         <div className='flash-message__content'>メモを作成しました</div>
-        <button className='flash-message__close' onClick={() => {}}>
+        <button className='flash-message__close' onClick={removeFlashMessage}>
           <FiX />
         </button>
       </div>
     </div>
   );
 }
+
+// {flashMessage.type === 'error' ? <FiXCircle /> : <FiCheckCircle />}
+// エラーの場合、FiXCircleのコンポーネントを表示。成功の場合FiCheckCircleのコンポーネントを表示。
