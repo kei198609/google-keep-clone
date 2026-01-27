65311034b56f8b3ffcd062d8b893532bfe6b3d50
yamaguchikei <keep_casting.19860910@hotmail.co.jp>
2026-01-27 19:45:13 +0900

    feat: restore current user on app initialization

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,32 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
 import FlashMessage from './components/FlashMessage';
+import { userCurrentUserStore } from './modules/auth/current-user.store'; //
+import { authRepository } from './modules/auth/auth.repository';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const { setCurrentUser } = userCurrentUserStore(); //グローバルステートに値を入れたいので、setCurrentUserを取得している。
+  const [isLoading,setIsLoading] = useState(true);
+
+  //fetchCurrentUserがコンポーネントが最初に表示されたタイミングで動かしたいのでuseEffectを使う。
+  useEffect(() => {
+    fetchCurrentUser();
+  }, []);
+
+  const fetchCurrentUser = async () => {
+    try {
+      const user = await authRepository.getCurrentUser()
+      setCurrentUser(user); //グローバルステートにuserの値をいれる
+    } catch (error) {
+      console.error(error);
+    } finally{
+      setIsLoading(false) //isLoadingは初期値はtrueになっているのでfalseにしている。
+    }
+  };
+
+  if(isLoading) return null; //isLoadingがtrueのままだったらnullを返す。isLoadingがtrueだと下層の処理に行かせないようにしている。
+
   return (
     <BrowserRouter>
       <Routes>
