06b5d3f1fa841f9d4452fad07f5ca31ba3b734fa
yamaguchikei <keep_casting.19860910@hotmail.co.jp>
2026-04-20 19:45:57 +0900

    feat: support grid and list layouts in NoteCard

--- src/pages/Home/NoteCard.tsx
+++ src/pages/Home/NoteCard.tsx
@@ -5,15 +5,19 @@ interface NoteCardProps {
   note: Note;
   onEdit: (note: Note) => void;
   onDelete: (id: string) => void;
+  viewMode: 'grid' | 'list';
 }
 
-export default function NoteCard({ note, onEdit, onDelete }: NoteCardProps) { // プロップスから受け取ってノートカードコンポーネントの中に伝えるようにする
+export default function NoteCard({ note, onEdit, onDelete, viewMode }: NoteCardProps) { // プロップスから受け取ってノートカードコンポーネントの中に伝えるようにする
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation(); //stopPropagationを呼ぶことで、ゴミ箱ボタンだけを押された処理だけを読んで、その後のイベントの処理を行える。ゴミ箱ボタンはノートカードに重なっているので、ノードカードをクリックした処理も一緒に走るのを防ぐ役割。
     onDelete(note.id);
   };
   return (
-    <div className='note-card' onClick={() => onEdit(note)}>
+    <div
+    className={viewMode === 'grid' ? 'note-card' : 'note-card note-card--list'}
+    onClick={() => onEdit(note)}
+    >
       { note.imageUrl && ( //imageUrlがある場合のみ、note-card__image-containerを表示
         <div className='note-card__image-container'>
           <img
